29a84afd5f0a4bdc6923a1151078add9b62cf46f
diasgc <gustavocdias@me.com>
2022-03-20 00:32:10 +0000

    upd

--- llvm.sh
+++ llvm.sh
@@ -4,22 +4,25 @@ lib='llvm'
 dsc='High quality encoder and decoder of the Audio Video Standard of China (AVS)'
 lic='GPL-2.0'
 src='https://github.com/llvm/llvm-project.git'
-bra='fir-dev'
+#bra='fir-dev'
 eta='30'
 
 cfg="cmake"
 cmake_shared='LIBCXX_ENABLE_SHARED'
 cmake_static='LIBCXX_ENABLE_STATIC'
-#cmake_config='-G Ninja -S runtimes -B build -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"'
+cmake_config='-DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_DOCS=OFF'
 
 extraOpts(){
     case $1 in
-        --llvm15)   bra='fir-dev';;
-        --llvm14)   bra='release/14.x';;
-        --llvm13)   bra='release/13.x';;
+        --15)   bra='fir-dev';;
+        --14)   bra='release/14.x';;
+        --13)   bra='release/13.x';;
         --cxx)      runtimes+=(libcxx);;
         --cxxabi)   runtimes+=(libcxxabi);;
         --unwind)   runtimes+=(libunwind);;
+        --crt)      runtimes+=(compiler-rt);;
+        --openmp)   runtimes+=(openmp);;
+        --all)      runtimes=(all);;
     esac
 }
 
@@ -29,6 +32,7 @@ lst_lib=''
 lst_bin=''
 lst_lic=''
 lst_pc=''
+pc_llib=void
 
 . xbuild
 
@@ -36,7 +40,7 @@ build_strip=false
 MAKE_EXECUTABLE=ninja
 dir_build="${dir_src}"
 cmake_bdir="-G Ninja -S runtimes -B build_${arch}"
-cmake_config="-DLLVM_ENABLE_RUNTIMES=$(str_concat ';' "${runtimes[@]}")"
+cmake_config+=" -DLLVM_ENABLE_RUNTIMES=$(str_concat ';' "${runtimes[@]}")"
 mkf="-C build_${arch} $(echo "${runtimes[@]//lib/ }")"
 mki="-C build_${arch} $(echo "${runtimes[@]//lib/install-}")"
 
