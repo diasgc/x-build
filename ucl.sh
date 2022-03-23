95980da2f3b00c3a848ecf385b44d6a8c1e4e53d
diasgc <gustavocdias@me.com>
2022-03-23 19:11:21 +0000

    ucl goes cmake

--- ucl.sh
+++ ucl.sh
@@ -5,68 +5,67 @@ dsc='UCL is a portable lossless data compression library written in ANSI C'
 lic='GPL-2.0'
 src='https://github.com/korczis/ucl.git'
 url='http://www.oberhumer.com/opensource/ucl'
-cfg='ac'
-eta='0'
+
+cfg='cmake'
+cmake_static='BUILD_STATIC_LIBS'
+cmake_bin='BUILD_PROGRAM'
 
 dev_vrs='1.03'
 
-pc_llib='-lucl'
+#cfg='ac'
+#pc_llib='-lucl'
+
 lst_inc='ucl/*.h'
 lst_lib='libucl'
 lst_bin=''
 lst_lic='COPYING README'
 lst_pc='ucl.pc'
+build_strip=false
+eta='14'
 
 . xbuild
 
 start
 
+# patch 01 create CMakeLists.txt with dual static/shared build support and
+# patch 02 create ucl.pc.in for pkgconfig support
+<<'LZS_PATCH'
+LQgEGEFkEMGsFMAyBLAzgF1QOnQD3QJACMAnAOwAMwFR1RoNAXBRcxVi516ANQ0sAoHsIgwEKDNjyEATBRkzqAZmAy
+loIgA5GMssyJYyAVhmkSANha8uAgAK3Q1ADQVeRJ0qOh7QgMYBbOHgAfX9kADtkfwBXf2CAJ3gAR2jkRIATAAoANQB
+RACUAZQBJAHkAOVAlLCMASiEAB3iAewAreF90TOjfABtQRABBcoBxAFVBkdzCiHqeVHguvKKyyoMaOeaG9GRm8MyAI
+THixAARYMKAFUHL4vBgxGKDmYAiACNU3vTQDGgd31AvWQb1QoGgvT2AHNQAB3ZDoAAWPwR0AyL1AFU2212+yOJ3OAA
+V8qURvlBpBQO9Pt8ms1IfFoP5QPBcB1ouhoG9evB0ZihEIAGbIbmZEaIUoHH7xXzBIEfPpS3wAegAVFgkahpaqsADN
+cq1b4EXMhSKxRLFcEEfBoOl4PFQRE+tFbUqer1tUb+TxHb1nSF0mkOuhmvFkPBUJkfX7jdFwp0cZkbelZcC3T8BbhA
+eFYHNQHmwelk3KGfEAJ6ZAAkAG8iaUAFK5cCXYLlcm5AC+1dQGfboGrvWzva70pT8t67dz+YW6GCHPikMWwVpDTtO3
+DlZrxIbTZbbc7Ve7uF7tYJBVu0wxY0uBKvu8guT7m/rjebrfvE6E+dAEV+vV6mWufIpkuGZq1rbdXz3Lse0/L980eA
+4yXyABNUBTmmW5W1uCpAWBWC4NAQZ8nAAAJYo8jQjDiiw1ZcLefC4PyMZylue9KKuaiblot4IgYuZ4HCdIBVjeM9ky
+OYhCTUc3UyF50TkuZkAFQ5jjOC4SKI3JzgQmZhlOUA8TUq4uPuHTJzzKS5Rk4Bfn+UBjNucB+MEpTxK9VzDMJYlSXJ
+cyC2TFk2Q5Ll4G6PoGmgXxYGZXBGQablUFdcLItgHUYrihKlV9HZenVPy5wXGcgWzUcSzDCM3QiqLQDdPyfw5P8AKI
+4CZkqlLQCYljijY9COJonCePCZyA2UiSeF8PYhUhaJEmCE1QrArcXzvDssAaXwsAiR8oEGABpXJggOTiUOCU5inyds
+lUW58dzfVb1u8CpEGQxTwl/f8ADETgvasdv2w7juQ07zsu66IJW9s1oBXrMK4nC5SVBpYEhCbwim173syL7EAvcBSg
+JZDqJGDrckGU4eqo/rKlQFFEiVdJmmVMHlruj9vTehrPu+0CDxHK0bTtVBexhzjsMqKMXWZ2693qAAdUBymaUBwngG
+FivgMF0GZQTQGaAVQHmgQQGAGq+ihzbwmIcgqBoOgGCINg2A4LhuD4IhBGEHhTdy9aLYIOQFGUVR1C0RgiHMfRDE0U
+wSAsKw+E4OwHGcVweHcIgvB8HgmngIVcAAXlsP6Duo4zEEQYIiVyL6AA1bCEQKZRzvP8+rZvkCPIQ5QDeJW6rRul0S
+PPLrlIQJfgHu+/bo8lXHr1ykZeBGG9oRTnDXxQ2xPZl7GcBEG/UFoFABoQ2C7lAWaVBUAS0F0j+I+Jv8HOr5xOiS1L
+WFQ3QdABO/cJCPKCUCAQgxj5EQMvBE38GioEYEqJUkJ4QImiG8HUzR/BKlgCGXwAAvNAQhsiCxxMvWwywSgVHrjwfI
+yRUiJBgaAIQVCUiBmwE0ZAAA3P4S96E8BQCCZewBED9mBD3XswBfR9CELwlhoYOE/2XkIcAApejQEhHQ4AxRqzjxEb
+BAQQA=
+LZS_PATCH
+
 # cpu av8 av7 x86 x64
-# NDK ++   .   .   .  clang
-# GNU  .   .   .   .  gcc
+# NDK +++ +++  .   .  clang
+# GNU  .   .   .   .  clang/gcc
 # WIN  .   .   .   .  clang/gcc
 
 # Filelist
 # --------
-# share/doc/ucl/COPYING
-# share/doc/ucl/README
+#
+# bin/uclpack
 # include/ucl/ucl.h
 # include/ucl/uclconf.h
 # include/ucl/uclutil.h
-# lib/pkgconfig/ucl.pc
 # lib/libucl.a
-# lib/libucl.la
 # lib/libucl.so
-
-<<'CMakeLists.txt'
-cmake_minimum_required(VERSION 3.5)
-project(ucl LANGUAGES C)
-
-option(BUILD_STATIC_LIBS "build static libs along with shared" ON)
-option(BUILD_PKGCONFIG "build pkgconfig file" OFF)
-
-files(GLOB src_libucl src/*.h src/*.c src/*.ch)
-files(GLOB src_headers include/ucl/*.h)
-
-function(add_libucl sfx lnk)
-    add_library(${PROJECT_NAME}${sfx} ${lnk} ${src_libucl})
-    set_target_properties(${PROJECT_NAME}${sfx} PROPERTIES OUTPUT_NAME ${PROJECT_NAME})
-    install(TARGETS ${PROJECT_NAME}${sfx}
-        LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-        ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-        RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
-    )
-endfunction()
-
-add_libucl("" "")
-if(BUILD_SHARED_LIBS AND BUILD_STATIC_LIBS)
-    add_libucl(-static STATIC)
-endif()
-
-if(BUILD_PKGCONFIG)
-    configure_file(../pkg-config.pc.in ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.pc @ONLY)
-    install(FILES ${CMAKE_BINARY_DIR}/SvtHevcEnc.pc DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
-endif()
-
-install(FILES ${src_headers} DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME}")
-CMakeLists.txt
+# lib/pkgconfig/ucl.pc
+# share/doc/ucl/COPYING
+# share/doc/ucl/README
