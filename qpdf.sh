3b827d081b8afacce4caf77b3bc1223dd2df70dc
diasgc <gustavocdias@me.com>
2022-03-30 21:14:55 +0100

    upd

--- qpdf.sh
+++ qpdf.sh
@@ -4,7 +4,7 @@ lib='qpdf'
 dsc='Primary QPDF source code and documentation'
 lic='Apache-2.0'
 src='https://github.com/qpdf/qpdf.git'
-cfg='ag'
+cfg='cmake'
 eta='480'
 ac_nosysroot=true
 
@@ -17,7 +17,8 @@ lst_pc=''
 dev_bra='master'
 dev_vrs=''
 
-build_prepare(){
+cmake_config='-Dfmt_lld_EXITCODE=0 -Dfmt_lld_EXITCODE__TRYRUN_OUTPUT=0' # -DUSE_INSECURE_RANDOM=1'
+_build_prepare(){
     local f=${dir_build}/seed.rnd
     head -c 1024 </dev/urandom >$f
     $host_cross && ac_config+=" --with-random=${f}"
