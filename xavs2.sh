10f97aebeca688927d92dcd7aefe94bc5a9ee0f9
diasgc <gustavocdias@me.com>
2024-02-23 00:14:08 +0000

    new xavs2

--- xavs2.sh
+++ xavs2.sh
@@ -3,10 +3,18 @@
 lib='xavs2'
 dsc='An open-source encoder of AVS2-P2/IEEE1857.4 video coding standard'
 lic='GPL-2.0'
-src='https://github.com/diasgc/xavs2.git'
 cfg='cmake'
+
+if [ "$cfg" = "ac" ]; then
+    src='https://github.com/pkuvcl/xavs2.git' 
+else
+    src='https://github.com/diasgc/xavs2.git' cfg='cmake'
+    cmake_config='-DDISABLE_WARNINGS=ON'
+    $host_x64 && cmake_config+=" -DENABLE_AVX=OFF"
+fi
+
 eta='40'
-bin='EXECUTABLE'
+bin='BUILD_EXECUTABLE'
 
 dev_vrs='1.3.232'
 
@@ -16,9 +24,13 @@ lst_bin='xavs2'
 lst_lic='LICENSE AUTHORS'
 lst_pc='xavs2.pc'
 
-cmake_config='-DDISABLE_WARNINGS=ON'
-
 . xbuild
+
+if [ "$cfg" = "ac" ]; then
+    $host_arm && ac_config+=" --disable-asm"
+    dir_config="${dir_src}/build/linux"
+    dir_build="${dir_src}/build/linux"
+fi
 
 start
 
