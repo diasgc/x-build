82f8974f40fea0a47e365a1f446bf0b7edfd772d
diasgc <gustavocdias@me.com>
2024-02-25 11:09:26 +0000

    generate scripts, svt-vp9

---
+++ svt-vp9.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# cpu av8 av7 x86 x64
+# NDK  .   .   .   .  clang
+# GNU  .   .   .   .  gcc
+# WIN  .   .   .   .  clang/gcc
+
+lib='svt-vp9'
+dsc='SVT VP9 encoder'
+lic='Other'
+vrs=''
+src="https://github.com/OpenVisualCloud/SVT-VP9"
+cfg='@cfg@'
+eta='0'
+
+dev_bra='master'
+dev_vrs=''
+stb_bra=''
+stb_vrs=''
+
+lst_inc=''
+lst_lib=''
+lst_bin=''
+lst_lic='LICENSE AUTHORS'
+lst_pc=''
+
+eta='20'
+
+. xbuild
+
+#source_get(){ return 0; }
+#patch_source(){ return 0; }
+#on_config_arm(){ return 0; }
+#on_config_x86x(){ return 0; }
+
+start
