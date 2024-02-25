85cf6fae775aa37bda407253037254672f2c14e9
diasgc <gustavocdias@me.com>
2024-02-25 21:52:58 +0000

    google lyra

---
+++ lyra.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# cpu av8 av7 x86 x64
+# NDK  .   .   .   .  clang
+# GNU  .   .   .   .  gcc
+# WIN  .   .   .   .  clang/gcc
+
+lib='lyra'
+dsc='A Very Low-Bitrate Codec for Speech Compression'
+lic='Apache License 2.0'
+vrs=''
+src="https://github.com/google/lyra.git"
+cfg='bazel'
+eta='0'
+
+dev_bra='master'
+dev_vrs=''
+stb_bra=''
+stb_vrs=''
+
+lst_inc=''
+lst_lib=''
+lst_bin=''
+lst_lic='LICENSE AUTHORS'
+lst_pc=''
+
+eta='20'
+
+. xbuild
+
+#source_get(){ return 0; }
+#patch_source(){ return 0; }
+#on_config_arm(){ return 0; }
+#on_config_x86x(){ return 0; }
+
+start
