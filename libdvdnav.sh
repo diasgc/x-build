5ebe52a3ba027855fc2d098c17ad80f7c230e817
diasgc <gustavocdias@me.com>
2024-03-04 13:26:54 +0000

    libdvdnav libqrencode

---
+++ libdvdnav.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# cpu av8 av7 x86 x64
+# NDK  .   .   .   .  clang
+# GNU  .   .   .   .  gcc
+# WIN  .   .   .   .  clang/gcc
+
+lib='libdvdnav'
+dsc='Library to navigate DVD disks'
+lic='GNU General Public License v2.0'
+vrs=''
+src="https://code.videolan.org/videolan/libdvdnav.git"
+cfg='cmake'
+eta='0'
+
+dev_bra='master'
+dev_vrs=''
+stb_bra=''
+stb_vrs=''
+
+lst_inc=''
+lst_lib=''
+lst_bin=''
+lst_lic='LICENSE AUTHORS'
+lst_pc=''
+
+eta='20'
+
+. xbuild
+
+#source_get(){ return 0; }
+#patch_source(){ return 0; }
+#on_config_arm(){ return 0; }
+#on_config_x86x(){ return 0; }
+
+start
---
+++ libqrencode.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# cpu av8 av7 x86 x64
+# NDK  .   .   .   .  clang
+# GNU  .   .   .   .  gcc
+# WIN  .   .   .   .  clang/gcc
+
+lib='libqrencode'
+dsc='A fast and compact QR Code encoding library'
+lic='GNU Lesser General Public License v2.1'
+vrs=''
+src="https://github.com/fukuchi/libqrencode.git"
+cfg='cmake'
+dep='libpng'
+eta='0'
+
+dev_bra='master'
+dev_vrs=''
+stb_bra=''
+stb_vrs=''
+
+lst_inc=''
+lst_lib=''
+lst_bin=''
+lst_lic='LICENSE AUTHORS'
+lst_pc=''
+
+eta='20'
+
+. xbuild
+
+#source_get(){ return 0; }
+#patch_source(){ return 0; }
+#on_config_arm(){ return 0; }
+#on_config_x86x(){ return 0; }
+
+start
