7edd9c51d3e4270697ff3d19cc98b5027182dfed
diasgc <gustavocdias@me.com>
2024-04-10 23:45:33 +0100

    lilv + patch subprojects missing wrap files

--- lilv.sh
+++ lilv.sh
@@ -5,17 +5,20 @@
 # WIN  .   .   .   .  clang/gcc
 
 lib='lilv'
+pkg='lilv-0'
 dsc='LV2 host library'
 lic='ISC License'
 vrs=''
 src="https://gitlab.com/lv2/lilv.git"
+#src='https://download.drobilla.net/lilv-0.24.24.tar.xz'
 cfg='meson'
-eta='0'
+dep='libiconv'
+eta='6'
 
-#meson_cfg='-Dbindings_py=disabled -Ddocs=disabled -Dhtml=disabled -Dsinglehtml=disabled -Dtests=disabled -Dtools=disabled'
+meson_cfg='-Dbindings_py=disabled -Ddocs=disabled -Dhtml=disabled -Dsinglehtml=disabled -Dtests=disabled -Dtools=disabled'
 
 dev_bra='master'
-dev_vrs=''
+dev_vrs='0.24.25'
 stb_bra=''
 stb_vrs=''
 
@@ -23,15 +26,12 @@ lst_inc=''
 lst_lib=''
 lst_bin=''
 lst_lic='LICENSE AUTHORS'
-lst_pc=''
+lst_pc='lilv-0.pc sratom-0.pc sord-0.pc serd-0.pc zix-0.pc'
 
 eta='20'
 
 . xbuild
 
-#source_get(){ return 0; }
-#patch_source(){ return 0; }
-#on_config_arm(){ return 0; }
-#on_config_x86x(){ return 0; }
+LDFLAGS+=" -liconv"
 
 start
