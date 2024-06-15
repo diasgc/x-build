d7c391888f0c2fb1965b40290667e75e513ea6db
diasgc <gustavocdias@me.com>
2024-06-15 13:36:21 +0100

    perl-cross

--- perl.sh
+++ perl.sh
@@ -7,16 +7,14 @@
 lib='perl'
 dsc='Perl'
 lic=''
-vrs=''
-src="http://www.cpan.org/src/5.0/perl-5.24.1.tar.gz"
 cfg='ac'
-eta='0'
+
 cross_vrs='1.1.3'
 
 dev_bra='master'
 dev_vrs=''
-stb_bra=''
-stb_vrs=''
+pkg_deb=''
+eta='0'
 
 lst_inc=''
 lst_lib=''
@@ -27,12 +25,18 @@ lst_pc=''
 eta='20'
 
 patch_source(){
-    curl -L -O https://github.com/arsv/perl-cross/releases/download/${cross_vrs}/perl-cross-${cross_vrs}.tar.gz
-    tar --strip-components=1 -zxf perl-cross-${cross_vrs}.tar.gz && rm -f perl-cross-${cross_vrs}.tar.gz
+    vrs_cross="$(tar_version 'https://github.com/arsv/perl-cross/releases' 'perl-cross-')"
+    src_cross="https://github.com/arsv/perl-cross/archive/refs/tags/${vrs_cross}.tar.gz"
+    pushdir ${dir_sources}
+    curl_tar_simple "${src_cross}"
+    popdir
 }
 
 on_config(){
-    if [ ${host_cross} ]; then
+    tar_stripcomponents=true
+    vrs="$(tar_version 'https://www.cpan.org/src/5.0/' 'perl-')"
+    src="http://www.cpan.org/src/5.0/perl-${vrs}.tar.gz"
+    if ${host_cross}; then
         cfg='ac'
         ac_config="--target=${arch} -Duseshrplib"
     fi
