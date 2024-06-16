15a4e7fdbd4969ccf12dc5277ed864118be8864a
diasgc <gustavocdias@me.com>
2024-06-16 17:54:24 +0100

    add cargo support

--- rav1e.sh
+++ rav1e.sh
@@ -7,27 +7,24 @@
 lib='rav1e'
 dsc='The fastest and safest AV1 encoder. '
 lic='BSD-2c'
-vrs=''
 src="https://github.com/xiph/rav1e.git"
+
 cfg='cargo'
 
 dev_bra='master'
 dev_vrs='0.7.1'
 pkg_deb='librav1e-dev'
-eta='528'
+eta='592'
+
+on_config(){
+    build_dir=$dir_src
+    cargo_cfg+=" --target ${arch}"
+}
 
 lst_inc=''
 lst_lib=''
 lst_bin=''
 lst_lic='LICENSE AUTHORS'
 lst_pc=''
-
-eta='528'
-
-build_all(){
-    pushd ${dir_src}
-    cargo cinstall --release --prefix ${dir_install}/rav1e --target ${arch}
-    popd
-}
 
 . xbuild && start
