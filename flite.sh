48a7949e7b4e88936e7e38642a7959cad8e22b2e
diasgc <gustavocdias@me.com>
2024-06-18 12:52:53 +0100

    upd

--- flite.sh
+++ flite.sh
@@ -3,27 +3,29 @@
 # ERROR: /usr/bin/install: cannot stat '../bin/flite_time': No such file or directory
 
 lib='flite'
-pkg_deb='flite'
 dsc='A small fast portable speech synthesis system'
 lic='GPL-2+'
-#src='https://github.com/festvox/flite.git' cfg='ac'
-vrs='v2.2'
-src="https://github.com/festvox/flite/archive/refs/tags/${vrs}.tar.gz" cfg='ac'
-eta='80'
-pc_llibs='-lflite -lflite_cmu_grapheme_lang \
-		 -lflite_cmu_grapheme_lex -lflite_cmu_indic_lang \
-		 -lflite_cmu_indic_lex -lflite_cmulex -lflite_cmu_time_awb \
-		 -lflite_cmu_us_awb -lflite_cmu_us_kal16 -lflite_cmu_us_kal \
-		 -lflite_cmu_us_rms -lflite_cmu_us_slt -lflite_usenglish'
-pc_libsprivate='-lm'
+src='https://github.com/festvox/flite.git'
+
+cfg='ac'
+WFLAGS='-Wno-void-pointer-to-int-cast
+ -Wno-int-to-void-pointer-cast
+ -Wno-absolute-value'
+
 dev_bra='main'
 dev_vrs=''
+pkg_deb='flite'
+eta='80'
 
 lst_inc=''
 lst_lib=''
 lst_bin=''
 lst_lic='copying'
-lst_pc=''
+lst_pc='flite.pc flite_cmu_grapheme_lang.pc
+ flite_cmu_grapheme_lex.pc flite_cmu_indic_lang.pc
+ flite_cmu_indic_lex.pc flite_cmulex.pc flite_cmu_time_awb.pc
+ flite_cmu_us_awb.pc flite_cmu_us_kal16.pc flite_cmu_us_kal.pc
+ lflite_cmu_us_rms.pc -lflite_cmu_us_slt.pc flite_usenglish.pc'
 
 _on_make(){
     make && make get_voices
@@ -35,6 +37,12 @@ on_config(){
 
 on_config_ndk(){
 	am_config="--with-langvox=android --with-audio=linux --target=${arch}"
+}
+
+on_create_pc(){
+	for pc in ${lst_pc}; do
+		build_pkgconfig --filename=${pc} --libs=-l${pc//\.pc/}
+	done
 }
 
 . xbuild && start
